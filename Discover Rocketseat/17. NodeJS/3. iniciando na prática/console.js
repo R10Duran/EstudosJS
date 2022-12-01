d051524e7ff718d262b4cca4fc34fd1c3ddcedbb
Robert Duran <robertwillian689@gmail.com>
2022-12-01 18:23:05 -0300

    att Rocketseat

---
+++ Discover Rocketseat/17. NodeJS/3. iniciando na prática/console.js
@@ -0,0 +1,9 @@
+console.log('Alguma coisa')
+
+//Diretório local, onde está o arquivo que estamos executando
+console.log(_dirname)
+
+//Caminho completo + nome do arquivo
+console.log(_filename)
+
+console.log(required('path')) // sempre precismos 
